2f16e2a426211bf26fce6f82e78c1bd8433292cd
Moneeb Sarmad <moneeb.sarmad@gmail.com>
2026-01-13 21:31:39 -0600

    Revert "Align portal dependencies and config"
    This reverts commit cb7e0c8266d9f8fdcb9c14fcd4ef6140fd852df2.

--- apps/portal/next.config.js
+++ apps/portal/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ["pdf-parse"],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
